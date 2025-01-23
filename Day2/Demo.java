db41c5669035a69feddc3d0d5ed856eb99ea85bb
Ashish Kumar Gupta <117809337+Ashish9956@users.noreply.github.com>
2025-01-23 13:48:39 +0530

    Add files via upload

---
+++ Day2/Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+public static void main(String arg[]){
+    Method md1= new Method();
+    md1.a=12;
+    md1.b=13;
+    md1.add();
+    md1.sub();}
+}
---
+++ Day2/Method.java
@@ -0,0 +1,9 @@
+class Method{
+     int a,b;
+     void add(){
+     System.out.println("Sum :" +(a+b));
+     }
+     void sub(){
+     System.out.println("diff :" +(a-b));
+     }
+}
