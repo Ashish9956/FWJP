886a1dc34f7c64f8371d11467a648a8327ddd087
Ashish Kumar Gupta <117809337+Ashish9956@users.noreply.github.com>
2025-01-27 17:33:19 +0530

    Update Day5Ques.java

--- Day5/Day5Ques.java
+++ Day5/Day5Ques.java
@@ -36,6 +36,30 @@ System.out.print(str);
 System.out.println("Number is zero : "+(n==0));
 System.out.println("Number is negative : "+(n<0));
 
+        // check number is even or odd
+       
+        System.out.print("Enter a number ");
+        int num=sc.nextInt();
+        
+        System.out.print(num%2==0?"Number is even" :" Number is odd");
+
+  //1. Check whether the character is alphabet or not
+ // 2. If character is alphabet then check if it is in lower case or upper case
+ //3. If it is in upper case then convert it into lower case
+//4. Check if it is vowel or not
+
+        System.out.print("Enter a character  ");
+        char  ch=sc.next().charAt(0);
+        boolean lc=ch>=97&&ch<=122;
+        boolean uc=ch>=65&&ch<=90;
+        boolean alp=lc||uc;
+        boolean vl=ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U';
+        System.out.println("It is alphabet : "+alp);
+        System.out.println("It lower case : "+lc);
+        System.out.println("It upper case : "+uc); 
+       System.out.print( uc?(ch+" It lower case is : "+(char)(ch+32)+'\n'):"");
+        System.out.println("It is vowel : "+vl);  
+
         
             }
 }
